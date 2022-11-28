f1527eaba4fd49050f612ba218cedc3de3ec68cf
Andrey Donskoy <donskoi.com@gmail.com>
2022-11-28 17:56:49 +0300

    update images

--- Adam Method/src/drawing.py
+++ Adam Method/src/drawing.py
@@ -11,7 +11,7 @@ def draw(all_x, all_y, h):
     y_real = [
         real_function(x) for x in x_real
     ]
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(5, 5))
     plt.title(f'Solution with h = {h}', size=20)
     plt.plot(all_x, all_y, c='black')
     plt.plot(x_real, y_real, c='red')
